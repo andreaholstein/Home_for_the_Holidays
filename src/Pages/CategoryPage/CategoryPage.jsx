7286439bfe3390efc3e866c9f987c8a8b40fda6a
Khushi Parekh <khushi4541@gmail.com>
2024-12-12 15:32:57 -0500

    styling changes

--- src/Pages/CategoryPage/CategoryPage.jsx
+++ src/Pages/CategoryPage/CategoryPage.jsx
@@ -12,12 +12,15 @@ import "./CategoryPage.scss";
 function CategoryPage({ handleSubmit }) {
 
     return (
-        <>
+        <section className="category__page">
+            <div className="category__div">
+                <img src="../../assets/images/category-image.jpeg" alt="tv screen" className="category__image" />
+            </div>
             <Link to="/MovieToWatch" >
                 <RandomButton handleSubmit={handleSubmit} />
             </Link>
-        </>
+        </section>
     )
 }
 
-export default CategoryPage
+export default CategoryPage;
