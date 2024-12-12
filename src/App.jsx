1d6cc2f8be972da14cc91f688f112ade6837f6a2
Khushi Parekh <khushi4541@gmail.com>
2024-12-12 12:27:01 -0500

    created and styled home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.scss'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './components/HomePage/HomePage'
+import Header from './components/Header/Header'
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
